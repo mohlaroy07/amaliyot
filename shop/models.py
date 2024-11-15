af4e1a34c4c9c7ce413993384543a9c62a468625
mohlaroy <mohlaroy@gmail.com>
2024-11-15 15:36:40 +0500

    mdg

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=244)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=244)
+    colour = models.CharField(max_length=244)
+    count = models.IntegerField()
+    price = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
+    count = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return self.count         
