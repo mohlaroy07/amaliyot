397ae5017a6a5c8137c232a4f854db4113d7f546
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-11-15 15:37:04 +0500

    asdsadsd

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Order, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
