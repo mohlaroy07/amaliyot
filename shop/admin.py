135223b4814e6168e5421d717b630986e53a69e1
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-11-15 15:42:56 +0500

    adas

--- shop/admin.py
+++ shop/admin.py
@@ -6,13 +6,14 @@ from .models import Category, Order, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["id", "name"]
+    list_display_links = ["name"]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["id", "name"]
+    list_display = ["id", "name", "category", "colour", "count", "price"]
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["id", "name"]
+    list_display = ["id", "product", "count"]
